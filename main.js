bfc0540a30b612625121c43f4f145ff7088cf0dd
Juan Jesus Tineo <jjtm1996@gmail.com>
2022-08-26 16:23:11 +0200

    add arrow efects in scroll

--- main.js
+++ main.js
@@ -5,7 +5,23 @@ const elements = {
     
     initButtons: function() {
         
+    },
+
+    initScrollEvent: function() {
+        $('.arrow').each((c, arrow) => {
+            !$(arrow).hasClass('active') 
+            && window.innerHeight + window.scrollY - 50 > arrow.offsetTop 
+            && $(arrow).addClass('active')
+        });
+        $(document).scroll(_ => {
+            $('.arrow').each((c, arrow) => {
+                !$(arrow).hasClass('active') 
+                && window.innerHeight + window.scrollY > arrow.offsetTop 
+                && $(arrow).addClass('active')
+            });
+        })
+        
     }
 }
 
-$(document).ready(() => elements.initButtons());
+$(document).ready(() => elements.initScrollEvent());
