5fff6ff716d3f8ad03fb500365a6b743bcff3da8
Juan Jesus Tineo <jjtm1996@gmail.com>
2022-10-25 10:41:13 +0200

    use bootstrap.min.css from node modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { router } from "./js/router"
 import { navbar } from "./js/navbar"
 import { footer } from "./js/footer"
 
-import './styles/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.css'
 
 router.init()
